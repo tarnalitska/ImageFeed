e4075cedce80540fb4c59dcb85a832f6ceec3ca9
Sofya <tarnalitska@gmail.com>
2025-04-01 14:00:36 +0200

    sprint_09 navigation-controller

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var userpick: UIImageView!
+
+    @IBOutlet private var fullName: UILabel!
+    @IBOutlet private var accountName: UILabel!
+    @IBOutlet private var bioText: UILabel!
+    
+    @IBOutlet private var exitButton: UIButton!
+    
+    @IBAction func exitClick(_ sender: Any) {
+    }
+    
+}
